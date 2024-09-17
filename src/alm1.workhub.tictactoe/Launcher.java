37d404f97148dfb8da6f93bd253172f55a92bf3c
Bronovytskyi Yevhen <zekanov16@gmail.com>
2024-09-17 22:18:53 +0300

    Add TerminalNumericKeypadCellNumberConverter

--- src/alm1.workhub.tictactoe/Launcher.java
+++ src/alm1.workhub.tictactoe/Launcher.java
@@ -1,12 +1,12 @@
 package alm1.workhub.tictactoe;
 
 import alm1.workhub.tictactoe.component.*;
-import alm1.workhub.tictactoe.keypad.DesktopNumericKeypadCellNumberConverter;
+import alm1.workhub.tictactoe.keypad.TerminalNumericKeypadCellNumberConverter;
 
 public final class Launcher {
 
     public static void main(String[] args) {
-        final CellNumberConverter cellNumberConverter = new DesktopNumericKeypadCellNumberConverter();
+        final CellNumberConverter cellNumberConverter = new TerminalNumericKeypadCellNumberConverter();
         Game game = new Game(new DataPrinter(cellNumberConverter),
                 new ComputerMove(), new UserMove(cellNumberConverter),
                 new WinnerVerifier(),
---
+++ src/alm1.workhub.tictactoe/keypad/TerminalNumericKeypadCellNumberConverter.java
@@ -0,0 +1,30 @@
+package alm1.workhub.tictactoe.keypad;
+
+import alm1.workhub.tictactoe.component.CellNumberConverter;
+import alm1.workhub.tictactoe.model.Cell;
+
+public class TerminalNumericKeypadCellNumberConverter implements CellNumberConverter {
+
+    private final char[][] mapping = {
+            {'1', '2', '3'},
+            {'4', '5', '6'},
+            {'7', '8', '9'}
+    };
+
+    @Override
+    public Cell toCell(final char number) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (mapping[i][j] == number) {
+                    return new Cell(i, j);
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public char toNumber(final Cell cell) {
+        return mapping[cell.getRow()][cell.getCol()];
+    }
+}
