1f293c39090d22a0b3531f0df721d4e28c446be7
Bronovytskyi Yevhen <zekanov16@gmail.com>
2024-09-17 22:13:27 +0300

    Convert CellNumberConverter class to interface

--- src/alm1.workhub.tictactoe/Launcher.java
+++ src/alm1.workhub.tictactoe/Launcher.java
@@ -1,11 +1,12 @@
 package alm1.workhub.tictactoe;
 
 import alm1.workhub.tictactoe.component.*;
+import alm1.workhub.tictactoe.keypad.DesktopNumericKeypadCellNumberConverter;
 
 public final class Launcher {
 
     public static void main(String[] args) {
-        final CellNumberConverter cellNumberConverter = new CellNumberConverter();
+        final CellNumberConverter cellNumberConverter = new DesktopNumericKeypadCellNumberConverter();
         Game game = new Game(new DataPrinter(cellNumberConverter),
                 new ComputerMove(), new UserMove(cellNumberConverter),
                 new WinnerVerifier(),
