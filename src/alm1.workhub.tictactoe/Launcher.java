194e9d7d6cd0afef22f63593bd48057996f2e8d0
Bronovytskyi Yevhen <zekanov16@gmail.com>
2024-09-17 15:00:01 +0300

    Implement random computer move

--- src/alm1.workhub.tictactoe/Launcher.java
+++ src/alm1.workhub.tictactoe/Launcher.java
@@ -6,5 +6,6 @@ public final class Launcher {
     public static void main(String[] args) {
         Game game = new Game(new DataPrinter(), new ComputerMove(), new UserMove(), new WinnerVerifier(), new DrawVerifier());
         game.play();
+
     }
 }
--- src/alm1.workhub.tictactoe/component/ComputerMove.java
+++ src/alm1.workhub.tictactoe/component/ComputerMove.java
@@ -1,9 +1,26 @@
 package alm1.workhub.tictactoe.component;
 
+import alm1.workhub.tictactoe.model.Cell;
 import alm1.workhub.tictactoe.model.GameTable;
+
+import java.util.Random;
 
 public class ComputerMove {
     public void make(final GameTable gameTable) {
+        final Random random = new Random();
+        while (true) {
+            final int row = random.nextInt(3);
+            final int col = random.nextInt(3);
+            final Cell randomCell = new Cell(row, col);
+            if (gameTable.isEmpty(randomCell)) {
+                gameTable.setSign(randomCell, '0');
+                return;
+            }
+        }
+
+
+
+
 
     }
 }
