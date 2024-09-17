11e9bfd462a5de3dbde93128339d787c5cb8ab6f
Bronovytskyi Yevhen <zekanov16@gmail.com>
2024-09-17 13:08:20 +0300

    Implement DataPrinter component

--- src/alm1.workhub.tictactoe/component/DataPrinter.java
+++ src/alm1.workhub.tictactoe/component/DataPrinter.java
@@ -1,13 +1,31 @@
 package alm1.workhub.tictactoe.component;
 
+import alm1.workhub.tictactoe.model.Cell;
 import alm1.workhub.tictactoe.model.GameTable;
 
 public class DataPrinter {
-    public void printMappingTable() {
 
+    public void printMappingTable() {
+        System.out.println(
+                "-------------\n" +
+                        "| 7 | 8 | 9 |\n" +
+                        "-------------\n" +
+                        "| 4 | 5 | 6 |\n" +
+                        "-------------\n" +
+                        "| 1 | 2 | 3 |\n" +
+                        "-------------\n"
+        );
     }
 
-    public void printGameTable(final GameTable gameTable) {
 
+    public void printGameTable(final GameTable gameTable) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("-------------");
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + gameTable.getSign(new Cell(i, j)) + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("-------------");
     }
 }
